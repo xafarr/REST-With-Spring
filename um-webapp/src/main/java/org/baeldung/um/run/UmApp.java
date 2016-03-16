fd8a2ea5b0252ceed9eaa1e0c9f8fa8389aacf30
Eugen Paraschiv <eugen@baeldung.com>
2016-03-16 14:11:55 -0500

    minor boot work and upgrades

--- um-webapp/src/main/java/org/baeldung/um/run/UmApp.java
+++ um-webapp/src/main/java/org/baeldung/um/run/UmApp.java
@@ -2,37 +2,40 @@ package org.baeldung.um.run;
 
 import org.baeldung.um.persistence.setup.MyApplicationContextInitializer;
 import org.baeldung.um.spring.UmContextConfig;
-import org.baeldung.um.spring.UmJavaSecurityConfig;
 import org.baeldung.um.spring.UmPersistenceJpaConfig;
 import org.baeldung.um.spring.UmServiceConfig;
 import org.baeldung.um.spring.UmServletConfig;
 import org.baeldung.um.spring.UmWebConfig;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
 import org.springframework.boot.autoconfigure.web.ErrorMvcAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication(exclude = { // @formatter:off
-        SecurityAutoConfiguration.class
-        , ErrorMvcAutoConfiguration.class
+        ErrorMvcAutoConfiguration.class
 })// @formatter:on
 public class UmApp extends SpringBootServletInitializer {
 
-    final static Class<?>[] CONFIGS = { // @formatter:off
+    private final static Object[] CONFIGS = { // @formatter:off
             UmContextConfig.class,
             UmPersistenceJpaConfig.class,
             UmServiceConfig.class,
             UmWebConfig.class,
             UmServletConfig.class,
-            UmJavaSecurityConfig.class
-    };// @formatter:on
+
+            UmApp.class
+    }; // @formatter:on
 
     //
 
     @Override
     protected SpringApplicationBuilder configure(final SpringApplicationBuilder application) {
-        return application.initializers(new MyApplicationContextInitializer()).sources(CONFIGS);
+        return application.sources(CONFIGS).initializers(new MyApplicationContextInitializer());
+    }
+
+    public static void main(final String... args) {
+        SpringApplication.run(CONFIGS, args);
     }
 
 }
