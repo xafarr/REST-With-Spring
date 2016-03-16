4382b879acab1560bd8e562f19bddf3bf9ce0329
Eugen Paraschiv <eugen@baeldung.com>
2016-03-16 15:18:23 -0500

    testing and spring boot work

--- um-webapp/src/main/java/org/baeldung/um/run/UmApp.java
+++ um-webapp/src/main/java/org/baeldung/um/run/UmApp.java
@@ -2,19 +2,18 @@ package org.baeldung.um.run;
 
 import org.baeldung.um.persistence.setup.MyApplicationContextInitializer;
 import org.baeldung.um.spring.UmContextConfig;
-import org.baeldung.um.spring.UmJavaSecurityConfig;
 import org.baeldung.um.spring.UmMetricConfig;
 import org.baeldung.um.spring.UmPersistenceJpaConfig;
 import org.baeldung.um.spring.UmServiceConfig;
 import org.baeldung.um.spring.UmServletConfig;
 import org.baeldung.um.spring.UmWebConfig;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.ErrorMvcAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication(exclude = { // @formatter:off
-        // SecurityAutoConfiguration.class,
         ErrorMvcAutoConfiguration.class
 })// @formatter:on
 public class UmApp extends SpringBootServletInitializer {
@@ -31,7 +30,7 @@ public class UmApp extends SpringBootServletInitializer {
             UmWebConfig.class,
             UmServletConfig.class,
 
-            UmJavaSecurityConfig.class
+            UmApp.class
     };// @formatter:on
 
     public UmApp() {
@@ -46,8 +45,8 @@ public class UmApp extends SpringBootServletInitializer {
     }
 
     public static void main(final String... args) {
-        // SpringApplication.run(CONFIGS, args);
-        new SpringApplicationBuilder(UmApp.class).initializers(new MyApplicationContextInitializer()).run(args);
+        SpringApplication.run(CONFIGS, args);
+        // new SpringApplicationBuilder(UmApp.class).initializers(new MyApplicationContextInitializer()).run(args);
     }
 
 }
