3c26fd97f8286671e17f0964b36f55ecfb95ea86
Eugen Paraschiv <eugen@baeldung.com>
2016-04-20 14:35:29 -0500

    minor error handling improvement

--- common/src/main/java/org/baeldung/common/web/RestPreconditions.java
+++ common/src/main/java/org/baeldung/common/web/RestPreconditions.java
@@ -2,12 +2,13 @@ package org.baeldung.common.web;
 
 import org.baeldung.common.web.exception.MyBadRequestException;
 import org.baeldung.common.web.exception.MyConflictException;
-import org.baeldung.common.web.exception.MyForbiddenException;
 import org.baeldung.common.web.exception.MyResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 
 /**
- * Simple static methods to be called at the start of your own methods to verify correct arguments and state. If the Precondition fails, an {@link HttpStatus} code is thrown
+ * Simple static methods to be called at the start of your own methods to verify
+ * correct arguments and state. If the Precondition fails, an {@link HttpStatus}
+ * code is thrown
  */
 public final class RestPreconditions {
 
@@ -22,15 +23,14 @@ public final class RestPreconditions {
      *
      * @param reference
      *            an object reference
+     *
      * @return the non-null reference that was validated
+     *
      * @throws MyResourceNotFoundException
      *             if {@code reference} is null
      */
     public static <T> T checkNotNull(final T reference) {
-        if (reference == null) {
-            throw new MyResourceNotFoundException();
-        }
-        return reference;
+        return checkNotNull(reference, null);
     }
 
     /**
@@ -38,13 +38,52 @@ public final class RestPreconditions {
      *
      * @param reference
      *            an object reference
+     * @param message
+     *            the message of the exception if the check fails
+     *
      * @return the non-null reference that was validated
+     *
+     * @throws MyResourceNotFoundException
+     *             if {@code reference} is null
+     */
+    public static <T> T checkNotNull(final T reference, final String message) {
+        if (reference == null) {
+            throw new MyResourceNotFoundException(message);
+        }
+        return reference;
+    }
+
+    /**
+     * Ensures that an object reference passed as a parameter to the calling
+     * method is not null.
+     *
+     * @param reference
+     *            an object reference
+     * @return the non-null reference that was validated
+     *
      * @throws MyBadRequestException
      *             if {@code reference} is null
      */
     public static <T> T checkRequestElementNotNull(final T reference) {
+        return checkRequestElementNotNull(reference, null);
+    }
+
+    /**
+     * Ensures that an object reference passed as a parameter to the calling method is not null.
+     *
+     * @param reference
+     *            an object reference
+     * @param message
+     *            the message of the exception if the check fails
+     *
+     * @return the non-null reference that was validated
+     *
+     * @throws MyBadRequestException
+     *             if {@code reference} is null
+     */
+    public static <T> T checkRequestElementNotNull(final T reference, final String message) {
         if (reference == null) {
-            throw new MyBadRequestException();
+            throw new MyBadRequestException(message);
         }
         return reference;
     }
@@ -59,8 +98,23 @@ public final class RestPreconditions {
      *             if {@code expression} is false
      */
     public static void checkRequestState(final boolean expression) {
+        checkRequestState(expression, null);
+    }
+
+    /**
+     * Ensures the truth of an expression
+     *
+     * @param expression
+     *            a boolean expression
+     * @param message
+     *            the message of the exception if the check fails
+     *
+     * @throws MyConflictException
+     *             if {@code expression} is false
+     */
+    public static void checkRequestState(final boolean expression, final String message) {
         if (!expression) {
-            throw new MyConflictException();
+            throw new MyConflictException(message);
         }
     }
 
@@ -74,52 +128,23 @@ public final class RestPreconditions {
      *             if {@code expression} is false
      */
     public static void checkIfBadRequest(final boolean expression) {
-        if (!expression) {
-            throw new MyBadRequestException();
-        }
+        checkIfBadRequest(expression, null);
     }
 
     /**
-     * Check if some value was found, otherwise throw exception.
+     * Ensures the truth of an expression related to the validity of the request
      *
      * @param expression
-     *            has value true if found, otherwise false
-     * @throws MyResourceNotFoundException
-     *             if expression is false, means value not found.
+     *            a boolean expression
+     * @param message
+     *            the message of the exception if the check fails
+     *
+     * @throws MyBadRequestException
+     *             if {@code expression} is false
      */
-    public static void checkFound(final boolean expression) {
+    public static void checkIfBadRequest(final boolean expression, final String message) {
         if (!expression) {
-            throw new MyResourceNotFoundException();
-        }
-    }
-
-    /**
-     * Check if some value was found, otherwise throw exception.
-     *
-     * @param expression
-     *            has value true if found, otherwise false
-     * @throws MyResourceNotFoundException
-     *             if expression is false, means value not found.
-     */
-    public static <T> T checkFound(final T resource) {
-        if (resource == null) {
-            throw new MyResourceNotFoundException();
-        }
-
-        return resource;
-    }
-
-    /**
-     * Check if some value was found, otherwise throw exception.
-     *
-     * @param expression
-     *            has value true if found, otherwise false
-     * @throws MyForbiddenException
-     *             if expression is false, means operation not allowed.
-     */
-    public static void checkAllowed(final boolean expression) {
-        if (!expression) {
-            throw new MyForbiddenException();
+            throw new MyBadRequestException(message);
         }
     }
 
