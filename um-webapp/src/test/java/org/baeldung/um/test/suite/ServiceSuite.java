3322f5d8658b8bd767687670d9348030ba6269e7
Eugen Paraschiv <eugen@baeldung.com>
2016-05-06 10:55:34 -0500

    cleanup work

--- um-webapp/src/test/java/org/baeldung/um/test/suite/ServiceSuite.java
+++ um-webapp/src/test/java/org/baeldung/um/test/suite/ServiceSuite.java
@@ -3,6 +3,7 @@ package org.baeldung.um.test.suite;
 import org.baeldung.um.service.main.PrincipalServiceIntegrationTest;
 import org.baeldung.um.service.main.PrivilegeServiceIntegrationTest;
 import org.baeldung.um.service.main.RoleServiceIntegrationTest;
+import org.baeldung.um.web.WebSpringIntegrationTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
@@ -11,7 +12,9 @@ import org.junit.runners.Suite.SuiteClasses;
 @SuiteClasses({ // @formatter:off
     PrincipalServiceIntegrationTest.class,
     PrivilegeServiceIntegrationTest.class,
-    RoleServiceIntegrationTest.class
+    RoleServiceIntegrationTest.class,
+
+    WebSpringIntegrationTest.class
 })
 // @formatter:on
 public final class ServiceSuite {
--- um-webapp/src/test/java/org/baeldung/um/web/WebSpringIntegrationTest.java
+++ um-webapp/src/test/java/org/baeldung/um/web/WebSpringIntegrationTest.java
@@ -1,10 +1,11 @@
 package org.baeldung.um.web;
 
-import org.baeldung.client.spring.CommonClientConfig;
-import org.baeldung.um.spring.UmClientConfig;
+import org.baeldung.um.run.UmApp;
 import org.baeldung.um.spring.UmContextConfig;
+import org.baeldung.um.spring.UmMetricConfig;
 import org.baeldung.um.spring.UmPersistenceJpaConfig;
 import org.baeldung.um.spring.UmServiceConfig;
+import org.baeldung.um.spring.UmServletConfig;
 import org.baeldung.um.spring.UmWebConfig;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -13,7 +14,15 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = { CommonClientConfig.class, UmPersistenceJpaConfig.class, UmContextConfig.class, UmServiceConfig.class, UmClientConfig.class, UmWebConfig.class })
+@ContextConfiguration(classes = { // @formatter:off
+        UmApp.class,
+        UmContextConfig.class,
+        UmPersistenceJpaConfig.class,
+        UmServiceConfig.class,
+        UmMetricConfig.class,
+        UmWebConfig.class,
+        UmServletConfig.class
+}) // @formatter:on
 @WebAppConfiguration
 public class WebSpringIntegrationTest {
 
